c66864ccf507e55c84050de8af89af290e5a1d60
Treinamento <marco.vieira@maps.com.br>
2016-03-07 21:26:35 -0300

    melhorando tratamento de erro

--- monolith/src/main/web/public/hello.js
+++ monolith/src/main/web/public/hello.js
@@ -1,17 +1,16 @@
 function Hello($scope, $http, $location) {
+	cnpjFundo = $location.search().cnpjFundo;
+	if (cnpjFundo == null) {
+		alert("Informar cnpj fundo. Ex: http://localhost:8080/#/?cnpjFundo=4");
+		return;
+	}
 
-cnpjFundo = $location.search().cnpjFundo;
-if (cnpjFundo == null) alert("Informar cnpj fundo. Ex: http://http://localhost:8080/#/?cnpjFundo=4");
-
-
-$http({
-  method: 'GET',
-  url: 'http://localhost:8081/fii/informes/mensal/' + cnpjFundo + '?competencia=01/2016'
-
-}).then(function successCallback(response) {
-	$scope.retorno = response.data;
-  }, function errorCallback(response) {
-	alert('response.status: ' + response.status);
-	alert('response.statusText: ' + response.statusText);
-  });
+	$http({
+		method: 'GET',
+		url: 'http://localhost:8081/fii/informes/mensal/' + cnpjFundo + '?competencia=01/2016'
+	}).then(function successCallback(response) {
+		$scope.retorno = response.data;
+	}, function errorCallback(response) {
+		alert('Falha ao chamar serviço de informe mensal FII. Response status: ' + response.status);
+	});
 }
