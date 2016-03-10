114e9fe790dd56b9ac539b9a15eebf18e8968769
Treinamento <marco.vieira@maps.com.br>
2016-03-09 23:26:28 -0300

    script para construir JARs e rodar docker

---
+++ docker-java8/sobeTudo.sh
@@ -0,0 +1,7 @@
+cd ~/dev/maps/monolith
+mvn package
+cd ~/dev/maps/fii-informe-mensal
+mvn package
+
+docker run -p 8080:8080 -d -v ~/dev/maps/monolith/target/monolith-0.1.0.jar:/home/app.jar -i -t java8
+docker run -p 8081:8080 -d -v ~/dev/maps/fii-informe-mensal/target/fii-informe-mensal-0.1.0.jar:/home/app.jar -i -t java8
